6b03942129df1b1117cd0dcd153b780b8fb9755e
ValeraBladee <potniyschenok@gmail.com>
2023-10-30 20:49:51 +0200

    +

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Formik, Form } from 'formik';
+import { nanoid } from 'nanoid';
+import * as Yup from 'yup';
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+import css from './Form.module.css';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Required'),
+  number: Yup.number().required(),
+});
+
+class ContactForm extends Component {
+  handleSubmit = (values, action) => {
+    const { name, number } = values;
+    if (name) {
+      const newContact = {
+        id: nanoid(),
+        name: name,
+        number: number,
+      };
+
+      const foo = () => this.props.newContact(newContact);
+      foo();
+    }
+    action.resetForm();
+  };
+
+  render() {
+    return (
+      <div className={css.wrapper}>
+        <Formik
+          validationSchema={schema}
+          initialValues={{
+            name: '',
+            number: '',
+          }}
+          onSubmit={this.handleSubmit}
+        >
+          <Form autoComplete="off">
+            <Input />
+            <Button type={'submit'} text={'Add contact'} />
+          </Form>
+        </Formik>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
